5d3dca6a77c7a7274fa13a31f9b71b6904da1b66
Shi Diyuan <shidiyuan@meituan.com>
2020-08-06 09:29:53 +0800

    small changes

--- src/js/utils/Axios.js
+++ src/js/utils/Axios.js
@@ -1,4 +1,4 @@
 import Axios from 'axios';
-Axios.defaults.baseURL = 'https://api.www.redhand.vip:8443';
+Axios.defaults.baseURL = 'https://api.www.redhand.vip';
 Axios.defaults.timeout = 5000;
 export default Axios;
